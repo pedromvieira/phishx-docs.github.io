245f1b61443d08a460196c9f2bb9d05e363af964
phishx-docs <pedro.vieira@phishx.io>
2020-08-11 20:56:18 -0300

    5

--- deploy.sh
+++ deploy.sh
@@ -11,13 +11,14 @@ rm -rf ../phishx-docs.github.io-temp/
 mkdir -p ../phishx-docs.github.io-temp/
 cp -R docs/.vuepress/dist/ ../phishx-docs.github.io-temp/
 
-# publish
+# pre commit
 git add .
 git commit -m 'pre-deploy'
 git push -u origin master
 
-
+# commit
 git checkout gh-pages
+cp -R ../phishx-docs.github.io-temp/ ./
 git add build/.
 git commit -m 'deploy'
 git push -u origin gh-pages
