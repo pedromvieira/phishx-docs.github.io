abd7cff78babe80d67b028cb0120f18789c7877d
phishx-docs <pedro.vieira@phishx.io>
2020-08-11 21:13:09 -0300

    pre-deploy

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run docs:build
+
+# copy
+rm -rf ../phishx-docs.github.io-temp/
+mkdir -p ../phishx-docs.github.io-temp/
+cp -R docs/.vuepress/dist/ ../phishx-docs.github.io-temp/
+
+# pre commit
+git add .
+git commit -m 'pre-deploy'
+git push -u origin master
--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,6 @@
 # abort on errors
 set -e
 
-# build
-npm run docs:build
-
-# copy
-rm -rf ../phishx-docs.github.io-temp/
-mkdir -p ../phishx-docs.github.io-temp/
-cp -R docs/.vuepress/dist/ ../phishx-docs.github.io-temp/
-
-# pre commit
-git add .
-git commit -m 'pre-deploy'
-git push -u origin master
-
 # commit
 git checkout gh-pages
 cp -R ../phishx-docs.github.io-temp/ ./
