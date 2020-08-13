dc0800b96622bdfe7644af13100d97f2ddd6d125
phishx-docs <pedro.vieira@phishx.io>
2020-08-13 12:26:59 -0300

    pre-deploy_20200813-12

--- build.sh
+++ build.sh
@@ -14,6 +14,6 @@ cp -R docs/.vuepress/dist/ ../phishx-docs.github.io-temp/
 # pre commit
 DATE=`date "+%Y%m%d-%H"`
 git add .
-git commit -m 'pre-deploy: $DATE'
+git commit -m pre-deploy_$DATE
 git push https://phishx-docs@github.com/phishx-docs/phishx-docs.github.io.git master
 git push
